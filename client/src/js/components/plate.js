4e44f9152bd61dcaaf83355a2f5b43b1b844ab20
Marc Juul <marc@juul.io>
2020-03-27 10:29:58 -0700

    row and column headers for 96-well plate

--- client/src/js/components/plate.js
+++ client/src/js/components/plate.js
@@ -10,7 +10,23 @@ class Plate extends Component {
     return (
       <div>
         <div id="my-plate" class="plate-96">
+        <div class="hrow header">
+        <div class="hcol"></div>
+        <div class="col col-1">1</div>
+        <div class="col col-2">2</div>
+        <div class="col col-3">3</div>
+        <div class="col col-4">4</div>
+        <div class="col col-5">5</div>
+        <div class="col col-6">6</div>
+        <div class="col col-7">7</div>
+        <div class="col col-8">8</div>
+        <div class="col col-9">9</div>
+        <div class="col col-10">10</div>
+        <div class="col col-11">11</div>
+        <div class="col col-12">12</div>
+        </div>
         <div class="row row-a">
+        <div class="hcol">A</div>
         <div class="col col-1"></div>
         <div class="col col-2"></div>
         <div class="col col-3"></div>
@@ -25,6 +41,7 @@ class Plate extends Component {
         <div class="col col-12"></div>
         </div>
         <div class="row row-b">
+        <div class="hcol">B</div>
         <div class="col col-1"></div>
         <div class="col col-2"></div>
         <div class="col col-3"></div>
@@ -39,6 +56,7 @@ class Plate extends Component {
         <div class="col col-12"></div>
         </div>
         <div class="row row-c">
+        <div class="hcol">C</div>
         <div class="col col-1"></div>
         <div class="col col-2"></div>
         <div class="col col-3"></div>
@@ -53,6 +71,7 @@ class Plate extends Component {
         <div class="col col-12"></div>
         </div>
         <div class="row row-d">
+        <div class="hcol">D</div>
         <div class="col col-1"></div>
         <div class="col col-2"></div>
         <div class="col col-3"></div>
@@ -67,6 +86,7 @@ class Plate extends Component {
         <div class="col col-12"></div>
         </div>
         <div class="row row-e">
+        <div class="hcol">E</div>
         <div class="col col-1"></div>
         <div class="col col-2"></div>
         <div class="col col-3"></div>
@@ -81,6 +101,7 @@ class Plate extends Component {
         <div class="col col-12"></div>
         </div>
         <div class="row row-f">
+        <div class="hcol">F</div>
         <div class="col col-1"></div>
         <div class="col col-2"></div>
         <div class="col col-3"></div>
@@ -95,6 +116,7 @@ class Plate extends Component {
         <div class="col col-12"></div>
         </div>
         <div class="row row-g">
+        <div class="hcol">G</div>
         <div class="col col-1"></div>
         <div class="col col-2"></div>
         <div class="col col-3"></div>
@@ -109,6 +131,7 @@ class Plate extends Component {
         <div class="col col-12"></div>
         </div>
         <div class="row row-h">
+        <div class="hcol">H</div>
         <div class="col col-1"></div>
         <div class="col col-2"></div>
         <div class="col col-3"></div>
