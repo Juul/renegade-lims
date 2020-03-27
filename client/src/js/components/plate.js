58303ce3e5c625af631761815ee5e8fc6bf94e6e
Marc Juul <marc@juul.io>
2020-03-27 10:46:52 -0700

    improved 96-well plate css/html

--- client/src/js/components/plate.js
+++ client/src/js/components/plate.js
@@ -27,123 +27,123 @@ class Plate extends Component {
         </div>
         <div class="row row-a">
         <div class="hcol">A</div>
-        <div class="col col-1"></div>
-        <div class="col col-2"></div>
-        <div class="col col-3"></div>
-        <div class="col col-4"></div>
-        <div class="col col-5"></div>
-        <div class="col col-6"></div>
-        <div class="col col-7"></div>
-        <div class="col col-8"></div>
-        <div class="col col-9"></div>
-        <div class="col col-10"></div>
-        <div class="col col-11"></div>
-        <div class="col col-12"></div>
+        <div class="col col-1"><div class="dot"></div></div>
+        <div class="col col-2"><div class="dot"></div></div>
+        <div class="col col-3"><div class="dot"></div></div>
+        <div class="col col-4"><div class="dot"></div></div>
+        <div class="col col-5"><div class="dot"></div></div>
+        <div class="col col-6"><div class="dot"></div></div>
+        <div class="col col-7"><div class="dot"></div></div>
+        <div class="col col-8"><div class="dot"></div></div>
+        <div class="col col-9"><div class="dot"></div></div>
+        <div class="col col-10"><div class="dot"></div></div>
+        <div class="col col-11"><div class="dot"></div></div>
+        <div class="col col-12"><div class="dot"></div></div>
         </div>
         <div class="row row-b">
         <div class="hcol">B</div>
-        <div class="col col-1"></div>
-        <div class="col col-2"></div>
-        <div class="col col-3"></div>
-        <div class="col col-4"></div>
-        <div class="col col-5"></div>
-        <div class="col col-6"></div>
-        <div class="col col-7"></div>
-        <div class="col col-8"></div>
-        <div class="col col-9"></div>
-        <div class="col col-10"></div>
-        <div class="col col-11"></div>
-        <div class="col col-12"></div>
+        <div class="col col-1"><div class="dot"></div></div>
+        <div class="col col-2"><div class="dot"></div></div>
+        <div class="col col-3"><div class="dot"></div></div>
+        <div class="col col-4"><div class="dot"></div></div>
+        <div class="col col-5"><div class="dot"></div></div>
+        <div class="col col-6"><div class="dot"></div></div>
+        <div class="col col-7"><div class="dot"></div></div>
+        <div class="col col-8"><div class="dot"></div></div>
+        <div class="col col-9"><div class="dot"></div></div>
+        <div class="col col-10"><div class="dot"></div></div>
+        <div class="col col-11"><div class="dot"></div></div>
+        <div class="col col-12"><div class="dot"></div></div>
         </div>
         <div class="row row-c">
         <div class="hcol">C</div>
-        <div class="col col-1"></div>
-        <div class="col col-2"></div>
-        <div class="col col-3"></div>
-        <div class="col col-4"></div>
-        <div class="col col-5"></div>
-        <div class="col col-6"></div>
-        <div class="col col-7"></div>
-        <div class="col col-8"></div>
-        <div class="col col-9"></div>
-        <div class="col col-10"></div>
-        <div class="col col-11"></div>
-        <div class="col col-12"></div>
+        <div class="col col-1"><div class="dot"></div></div>
+        <div class="col col-2"><div class="dot"></div></div>
+        <div class="col col-3"><div class="dot"></div></div>
+        <div class="col col-4"><div class="dot"></div></div>
+        <div class="col col-5"><div class="dot"></div></div>
+        <div class="col col-6"><div class="dot"></div></div>
+        <div class="col col-7"><div class="dot"></div></div>
+        <div class="col col-8"><div class="dot"></div></div>
+        <div class="col col-9"><div class="dot"></div></div>
+        <div class="col col-10"><div class="dot"></div></div>
+        <div class="col col-11"><div class="dot"></div></div>
+        <div class="col col-12"><div class="dot"></div></div>
         </div>
         <div class="row row-d">
         <div class="hcol">D</div>
-        <div class="col col-1"></div>
-        <div class="col col-2"></div>
-        <div class="col col-3"></div>
-        <div class="col col-4"></div>
-        <div class="col col-5"></div>
-        <div class="col col-6"></div>
-        <div class="col col-7"></div>
-        <div class="col col-8"></div>
-        <div class="col col-9"></div>
-        <div class="col col-10"></div>
-        <div class="col col-11"></div>
-        <div class="col col-12"></div>
+        <div class="col col-1"><div class="dot"></div></div>
+        <div class="col col-2"><div class="dot"></div></div>
+        <div class="col col-3"><div class="dot"></div></div>
+        <div class="col col-4"><div class="dot"></div></div>
+        <div class="col col-5"><div class="dot"></div></div>
+        <div class="col col-6"><div class="dot"></div></div>
+        <div class="col col-7"><div class="dot"></div></div>
+        <div class="col col-8"><div class="dot"></div></div>
+        <div class="col col-9"><div class="dot"></div></div>
+        <div class="col col-10"><div class="dot"></div></div>
+        <div class="col col-11"><div class="dot"></div></div>
+        <div class="col col-12"><div class="dot"></div></div>
         </div>
         <div class="row row-e">
         <div class="hcol">E</div>
-        <div class="col col-1"></div>
-        <div class="col col-2"></div>
-        <div class="col col-3"></div>
-        <div class="col col-4"></div>
-        <div class="col col-5"></div>
-        <div class="col col-6"></div>
-        <div class="col col-7"></div>
-        <div class="col col-8"></div>
-        <div class="col col-9"></div>
-        <div class="col col-10"></div>
-        <div class="col col-11"></div>
-        <div class="col col-12"></div>
+        <div class="col col-1"><div class="dot"></div></div>
+        <div class="col col-2"><div class="dot"></div></div>
+        <div class="col col-3"><div class="dot"></div></div>
+        <div class="col col-4"><div class="dot"></div></div>
+        <div class="col col-5"><div class="dot"></div></div>
+        <div class="col col-6"><div class="dot"></div></div>
+        <div class="col col-7"><div class="dot"></div></div>
+        <div class="col col-8"><div class="dot"></div></div>
+        <div class="col col-9"><div class="dot"></div></div>
+        <div class="col col-10"><div class="dot"></div></div>
+        <div class="col col-11"><div class="dot"></div></div>
+        <div class="col col-12"><div class="dot"></div></div>
         </div>
         <div class="row row-f">
         <div class="hcol">F</div>
-        <div class="col col-1"></div>
-        <div class="col col-2"></div>
-        <div class="col col-3"></div>
-        <div class="col col-4"></div>
-        <div class="col col-5"></div>
-        <div class="col col-6"></div>
-        <div class="col col-7"></div>
-        <div class="col col-8"></div>
-        <div class="col col-9"></div>
-        <div class="col col-10"></div>
-        <div class="col col-11"></div>
-        <div class="col col-12"></div>
+        <div class="col col-1"><div class="dot"></div></div>
+        <div class="col col-2"><div class="dot"></div></div>
+        <div class="col col-3"><div class="dot"></div></div>
+        <div class="col col-4"><div class="dot"></div></div>
+        <div class="col col-5"><div class="dot"></div></div>
+        <div class="col col-6"><div class="dot"></div></div>
+        <div class="col col-7"><div class="dot"></div></div>
+        <div class="col col-8"><div class="dot"></div></div>
+        <div class="col col-9"><div class="dot"></div></div>
+        <div class="col col-10"><div class="dot"></div></div>
+        <div class="col col-11"><div class="dot"></div></div>
+        <div class="col col-12"><div class="dot"></div></div>
         </div>
         <div class="row row-g">
         <div class="hcol">G</div>
-        <div class="col col-1"></div>
-        <div class="col col-2"></div>
-        <div class="col col-3"></div>
-        <div class="col col-4"></div>
-        <div class="col col-5"></div>
-        <div class="col col-6"></div>
-        <div class="col col-7"></div>
-        <div class="col col-8"></div>
-        <div class="col col-9"></div>
-        <div class="col col-10"></div>
-        <div class="col col-11"></div>
-        <div class="col col-12"></div>
+        <div class="col col-1"><div class="dot"></div></div>
+        <div class="col col-2"><div class="dot"></div></div>
+        <div class="col col-3"><div class="dot"></div></div>
+        <div class="col col-4"><div class="dot"></div></div>
+        <div class="col col-5"><div class="dot"></div></div>
+        <div class="col col-6"><div class="dot"></div></div>
+        <div class="col col-7"><div class="dot"></div></div>
+        <div class="col col-8"><div class="dot"></div></div>
+        <div class="col col-9"><div class="dot"></div></div>
+        <div class="col col-10"><div class="dot"></div></div>
+        <div class="col col-11"><div class="dot"></div></div>
+        <div class="col col-12"><div class="dot"></div></div>
         </div>
         <div class="row row-h">
         <div class="hcol">H</div>
-        <div class="col col-1"></div>
-        <div class="col col-2"></div>
-        <div class="col col-3"></div>
-        <div class="col col-4"></div>
-        <div class="col col-5"></div>
-        <div class="col col-6"></div>
-        <div class="col col-7"></div>
-        <div class="col col-8"></div>
-        <div class="col col-9"></div>
-        <div class="col col-10"></div>
-        <div class="col col-11"></div>
-        <div class="col col-12"></div>
+        <div class="col col-1"><div class="dot"></div></div>
+        <div class="col col-2"><div class="dot"></div></div>
+        <div class="col col-3"><div class="dot"></div></div>
+        <div class="col col-4"><div class="dot"></div></div>
+        <div class="col col-5"><div class="dot"></div></div>
+        <div class="col col-6"><div class="dot"></div></div>
+        <div class="col col-7"><div class="dot"></div></div>
+        <div class="col col-8"><div class="dot"></div></div>
+        <div class="col col-9"><div class="dot"></div></div>
+        <div class="col col-10"><div class="dot"></div></div>
+        <div class="col col-11"><div class="dot"></div></div>
+        <div class="col col-12"><div class="dot"></div></div>
         </div>
         </div>
       </div>
